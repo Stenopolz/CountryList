2620596a8a0f37ea99e9f03a9c6fc24bf6a49527
Pavel Shchors <pavel.shchors@gmail.com>
2022-09-18 22:47:06 +0200

    Remove unused files

--- app/src/main/java/com/stenopolz/countrylist/model/data/Country.kt
+++
@@ -1,8 +0,0 @@
-package com.stenopolz.countrylist.model.data
-
-import com.squareup.moshi.Json
-
-data class Country(
-    @field:Json(name = "name")
-    val name: CountryName
-)
--- app/src/main/java/com/stenopolz/countrylist/model/data/CountryName.kt
+++
@@ -1,8 +0,0 @@
-package com.stenopolz.countrylist.model.data
-
-import com.squareup.moshi.Json
-
-data class CountryName(
-    @field:Json(name = "official")
-    val official: String
-)
