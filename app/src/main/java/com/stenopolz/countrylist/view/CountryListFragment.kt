94161f53f280ecc7287e7219fcc70b7b9ceb769f
Pavel Shchors <pavel.shchors@gmail.com>
2022-09-21 19:41:39 +0200

    Add list item divider

--- app/src/main/java/com/stenopolz/countrylist/view/CountryListFragment.kt
+++ app/src/main/java/com/stenopolz/countrylist/view/CountryListFragment.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.stenopolz.countrylist.R
 import com.stenopolz.countrylist.databinding.FragmentCountryListBinding
 import com.stenopolz.countrylist.viewmodel.CountryListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class CountryListFragment : Fragment() {
@@ -44,6 +48,16 @@ class CountryListFragment : Fragment() {
                 countryListViewModel.onCountryIdClicked(it)
             }
             viewModel = countryListViewModel
+            list.addItemDecoration(
+                DividerItemDecoration(
+                    requireContext(),
+                    DividerItemDecoration.VERTICAL
+                ).apply {
+                    AppCompatResources.getDrawable(requireContext(), R.drawable.divider)?.let {
+                        setDrawable(it)
+                    }
+                }
+            )
         }
 
         lifecycleScope.launch {
