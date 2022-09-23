1f5c746f90cacdd1ed7c6a18a42f131ed66a1d19
Pavel Shchors <pavel@fishbrain.com>
2022-09-23 14:59:54 +0300

    Clean Main Activity imports

--- app/src/main/java/com/stenopolz/countrylist/view/MainActivity.kt
+++ app/src/main/java/com/stenopolz/countrylist/view/MainActivity.kt
@@ -1,24 +1,14 @@
 package com.stenopolz.countrylist.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.stenopolz.countrylist.R
 import com.stenopolz.countrylist.databinding.ActivityMainBinding
-import com.stenopolz.countrylist.extensions.apiCall
-import com.stenopolz.countrylist.model.service.CountryApi
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
